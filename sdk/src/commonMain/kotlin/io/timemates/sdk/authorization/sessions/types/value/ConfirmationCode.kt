0e330324747bff1455b64bd2cc695e8f013d4192
y9vad9 <y9vad9@gmail.com>
2023-06-11 16:15:48 +0200

    fix: invalid ConfirmationCode validator

--- sdk/src/commonMain/kotlin/io/timemates/sdk/authorization/sessions/types/value/ConfirmationCode.kt
+++ sdk/src/commonMain/kotlin/io/timemates/sdk/authorization/sessions/types/value/ConfirmationCode.kt
@@ -7,13 +7,11 @@ import io.timemates.sdk.common.constructor.Factory
 public value class ConfirmationCode private constructor(public val string: String) {
     public companion object : Factory<ConfirmationCode, String>() {
         public const val SIZE: Int = 6
-        public val PATTERN: Regex = Regex("^[0-9]+$")
 
         override fun create(input: String): Result<ConfirmationCode> {
             return when {
-                input.isBlank() -> Result.failure(CreationFailure.ofBlank())
+                input.isBlank() -> Result.failure(CreationFailure.ofBlankValue())
                 input.length != SIZE -> Result.failure(CreationFailure.ofSizeExact(SIZE))
-                !input.matches(PATTERN) -> Result.failure(CreationFailure.ofPattern(PATTERN))
                 else -> Result.success(ConfirmationCode(input))
             }
         }
