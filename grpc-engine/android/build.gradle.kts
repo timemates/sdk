bd6baff8489fa225bff8c263689705233be8c48c
y9vad9 <y9vad9@gmail.com>
2023-07-10 13:28:41 +0200

    fix: componentName for `grpc-engine-android` publication

--- grpc-engine/android/build.gradle.kts
+++ grpc-engine/android/build.gradle.kts
@@ -33,7 +33,7 @@ deployLibrary {
         deployPath = System.getenv("TIMEMATES_SSH_DEPLOY_PATH")
 
         group = "io.timemates"
-        componentName = "java"
+        componentName = "bundleReleaseAar"
         artifactId = "grpc-engine-android"
         name = "grpc-engine-android"
 
